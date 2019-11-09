f6b5f455d770dddda65bce2330595bc6979ea5f0
nbaledio <nbaledio@ucsc.edu>
2019-11-08 16:50:06 -0800

    Updating wizard beam
    -Beam now has two types of hitboxes:
    a) Just a rectangle
    b) A scrolling rectangle
    (Scrolling is currently enabled)

--- game/js/prefabs/wizard.js
+++ game/js/prefabs/wizard.js
@@ -6,7 +6,10 @@ function wizard(){
 	this.direction = 'right';
 	this.attack_hitbox;
 	this.special_hitbox;
+	this.scrolling = false;
+	this.special_direction = 'right';
 	this.cooldown = false;
+	this.x = 0;
 }
 
 wizard.prototype = {
@@ -68,7 +71,8 @@ wizard.prototype = {
 	special: function(game,enemy,pad){
 		//Checks for button input
 		if (game.input.keyboard.justPressed(Phaser.Keyboard.X) || pad.justPressed(Phaser.Gamepad.XBOX360_Y)){
-			if (this.cooldown == false) {
+			//Instant Beam
+			/*if (this.cooldown == false) {
 				this.cooldown = true;
 				//Checks if player is facing right or left and places hitbox accordingly
 				if(this.direction == 'right'){
@@ -84,6 +88,53 @@ wizard.prototype = {
 				}
 				game.debug.body(this.special_hitbox);
 				//Destroy hitbox
+				this.special_hitbox.destroy();
+				game.time.events.add(Phaser.Timer.SECOND * 5, this.resetCooldown, this);
+				console.log('Player 1 Special on cooldown');
+			}*/
+			
+			//Kamehameha style (Scrolling)
+			//Check Direction player is facing
+			if(this.cooldown == false && this.scrolling == false){
+				if(this.direction == 'right'){
+					this.special_hitbox = game.add.sprite(this.sprite.x + 130,this.sprite.y + 40,null);
+					this.special_direction = 'right';
+				}else{
+					this.special_hitbox = game.add.sprite(this.sprite.x,this.sprite.y + 40,null);
+					this.special_direction = 'left';
+				}
+				this.scrolling = true;
+				//Enable hitbox
+				game.physics.arcade.enable(this.special_hitbox);
+				this.special_hitbox.body.setSize(0,100);
+			}
+		}
+		//Scroll beam to the right
+		if(this.scrolling == true && this.cooldown == false && this.special_direction == 'right'){
+			if( this.x < 1980){
+				this.x += 20;
+				this.special_hitbox.body.setSize(this.x,100);
+				game.debug.body(this.special_hitbox);
+			}else{
+				this.x = 0;
+				this.scrolling = false;
+				this.cooldown = true;
+				this.special_hitbox.destroy();
+				game.time.events.add(Phaser.Timer.SECOND * 5, this.resetCooldown, this);
+				console.log('Player 1 Special on cooldown');
+			}
+		}
+		//Scroll beam to the left
+		if(this.scrolling == true && this.cooldown == false && this.special_direction == 'left'){
+			if( this.x < 1980){
+				this.x += 20;
+				this.special_hitbox.x -= 20;
+				this.special_hitbox.body.setSize(this.x,100);
+				game.debug.body(this.special_hitbox);
+			}else{
+				this.x = 0;
+				this.scrolling = false;
+				this.cooldown = true;
 				this.special_hitbox.destroy();
 				game.time.events.add(Phaser.Timer.SECOND * 5, this.resetCooldown, this);
 				console.log('Player 1 Special on cooldown');
